5fa2e2adf19f796dd1f6ebec0577f934dfde4643
Stacy <hurrendor@gmail.com>
2024-11-18 14:18:29 -0800

    Formatting

--- index.tsx
+++ index.tsx
@@ -1,31 +1,28 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import StartApp from './start/StartApp';
-import EndApp from './end/EndApp';
-import ErrorPage from './ErrorPage';
-
+import StartApp from "./start/StartApp";
+import EndApp from "./end/EndApp";
+import ErrorPage from "./ErrorPage";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-)
+  document.getElementById("root") as HTMLElement
+);
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: (
-            <StartApp />
-        ),
-        errorElement: (<ErrorPage />)
-    },
-    {
-        path: "end",
-        element: (<EndApp />),
-        errorElement: (<ErrorPage />)
-    }
+  {
+    path: "/",
+    element: <StartApp />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/end",
+    element: <EndApp />,
+    errorElement: <ErrorPage />,
+  },
 ]);
 
 root.render(
-        <>
-        <RouterProvider router={router} />
-        </>
+  <>
+    <RouterProvider router={router} />
+  </>
 );
